5d023f0755fdde04fedc5a2a7bf69fdbb58b8c0e
Aimtbr <maxim.marchuk.d@gmail.com>
2019-03-24 21:00:19 +0200

    Updates

---
+++ precache-manifest.8869032ddf86cb7c3210ae7941799b55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "43e704bb608090a0dded",
+    "url": "/telegramcontest/static/js/runtime~main.240a4a9f.js"
+  },
+  {
+    "revision": "7e97f2f7f9315befa4fc",
+    "url": "/telegramcontest/static/js/main.1cc69cbf.chunk.js"
+  },
+  {
+    "revision": "09891e9880d22aea70c1",
+    "url": "/telegramcontest/static/js/2.3031a66a.chunk.js"
+  },
+  {
+    "revision": "7e97f2f7f9315befa4fc",
+    "url": "/telegramcontest/static/css/main.377284b2.chunk.css"
+  },
+  {
+    "revision": "09891e9880d22aea70c1",
+    "url": "/telegramcontest/static/css/2.ae5f9a7c.chunk.css"
+  },
+  {
+    "revision": "4961d6eb60c0ebb6f19d45a99f3bbb60",
+    "url": "/telegramcontest/index.html"
+  }
+];
